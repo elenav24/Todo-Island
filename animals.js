e2e75f7f4be368c225ac0c2ca112b36ee508de1d
thisguyblink <patrickliamsmith@gmail.com>
2024-02-24 20:51:48 -0800

    Added Animals.js and Image

---
+++ animals.js
@@ -0,0 +1,15 @@
+export const animals = [
+    {
+        name: "Joe Lee",
+        species: "Giant Panda",
+        cost: 750,
+        fun_fact: "Likes Pizza and Swimming"
+    },
+    {
+        name: "Nate ",
+        species: "Lions",
+        cost: 250,
+        fun_fact: "Likes Roaring"
+    }
+]
+console.log(animals);
