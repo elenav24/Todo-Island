9fe810a539d110e036fa6c070dfda10e76bcd905
Elena Marquez <elena-marquez@csu.fullerton.edu>
2024-02-25 14:42:33 -0800

    updated

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
-
-//document.getElementById('output').innerHTML = window.coin;
+window.coin = 100000;
+// window.coin = document.getElementById('output').innerHTML;
 function showPanda() {
     if (window.coin >= 750){
         localStorage.setItem('pandaStyle', 'true');
@@ -8,7 +8,6 @@ function showPanda() {
         document.getElementById('output').innerHTML = coin;
     }
 }
-
 function showDog() {
     if (window.coin >= 500) {
         localStorage.setItem('dogStyle', 'true');
@@ -17,7 +16,30 @@ function showDog() {
         document.getElementById('output').innerHTML = coin;
     }
 }
-
+function showCat() {
+    if (window.coin >= 600) {
+        localStorage.setItem('catStyle', 'true');
+        window.coin -= 600;
+        console.log(coin);
+        document.getElementById('output').innerHTML = coin;
+    }
+}
+function showDaisy() {
+    if (window.coin >= 300) {
+        localStorage.setItem('daistStyle', 'true');
+        window.coin -= 300;
+        console.log(coin);
+        document.getElementById('output').innerHTML = coin;
+    }
+}
+function showSunflower() {
+    if (window.coin >= 350) {
+        localStorage.setItem('sunflowerStyle', 'true');
+        window.coin -= 350;
+        console.log(coin);
+        document.getElementById('output').innerHTML = coin;
+    }
+}
 
 
 document.getElementById('output').innerHTML = coins;
