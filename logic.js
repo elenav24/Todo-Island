fe5826c8fa1bcde44727c54312ace56ddb64094e
thisguyblink <patrickliamsmith@gmail.com>
2024-02-25 12:17:48 -0800

    Changes

--- logic.js
+++ logic.js
@@ -1,7 +1,19 @@
+
+var coins = 1000;
 function showPanda() {
-    localStorage.setItem('pandaStyle', 'true');
+    if (coins >= 750){
+        localStorage.setItem('pandaStyle', 'true');
+        coins -= 750;
+        document.getElementById('output').innerHTML = coins;
+    }
 }
 
 function showDog() {
     localStorage.setItem('dogStyle', 'true');
 }
+
+
+document.getElementById('output').innerHTML = coins;
+
+// Update the content of the output element with the JavaScript value
+// outputElement.textContent = coins;
