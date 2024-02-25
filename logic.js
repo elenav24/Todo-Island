2fec5d08d7cae29548b5b662250e551a48e2e20f
Elena Marquez <elena-marquez@csu.fullerton.edu>
2024-02-25 06:40:43 -0800

    deleted comments

--- logic.js
+++ logic.js
@@ -1,34 +1,3 @@
-// // document.getElementById('pandaButton').addEventListener('click', function() {
-// //     // Store a value in localStorage to indicate that button 1 was clicked
-// //     localStorage.setItem('pandaButton', 'true');
-// //     // Redirect to button2.html
-// //     window.location.href = 'shop.html';
-// // });
-
-// // document.getElementById('pandaButton').addEventListener('click', function() {
-// //     // Retrieve the value from localStorage set by button1.html
-// //     var button1Clicked = localStorage.getItem('pandaButton');
-// //     // Do something based on whether button 1 was clicked
-// //     var element = document.getElementById('pandaPic');
-// //     if (button1Clicked === 'true') {
-// //         element.style.opacity = '.7';
-// //         // alert('Button 1 was clicked before!');
-// //     } else {
-// //         alert('Button 1 was not clicked before.');
-// //     }
-// // });
-// const profile = document.querySelector('#profile');
-// const title = document.querySelector('#Title');
-// profile.addEventListener('click', () => {
-//     profile.style.color = 'green';
-//     console.log(title.style)
-// })
-// document.getElementById('#profile').addEventListener('click', function() {
-//     var myElement = document.getElementById('#Title');
-//         // myElement.style.opacity = 1 ; // Change opacity to 0.2 if it's currently 1
-//         myElement.style.color = white;
-//         console.log(myElement.style)
-// });
 function showPhoto() {
     var photoElement = document.getElementById("pandaPic");
 
